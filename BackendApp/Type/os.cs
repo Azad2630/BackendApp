fc0a89653f4eba98fbf4a1151b35edda1ce0644b
Tec <Tec@SKAB1-PC1>
2023-08-29 09:55:15 +0200

    Add project files.

---
+++ BackendApp/Type/os.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendApp.Type
+{
+    internal class os
+    {
+        public string UserFullName { get; set; }
+
+        public os(string userFullName)
+        {
+            UserFullName = userFullName;
+        }
+    }
+}
---
+++ BackendApp/Type/windows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendApp.Type
+{
+    internal class windows : os, IOS
+    {
+        public windows(string userfullname) : base(userfullname)
+        {
+
+        }
+        public string ShowOSType()
+        {
+            return $"welcome {UserFullName} to windows, running from backend!";
+        }
+    }
+}
